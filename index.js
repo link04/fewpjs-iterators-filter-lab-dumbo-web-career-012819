5bb93446e7e8c2f104877cbb74328712776d041b
Maximo Bautista <max.bautista97@gmail.com>
2019-03-14 14:03:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+const findMatching = (array, string) => {
+  return array.filter(element => element.toLowerCase() == string.toLowerCase() );
+};
+
+const fuzzyMatch = (array, string) => {
+  return array.filter(element => element.startsWith(string))
+};
+
+const matchName = (array, string) => {
+  return array.filter(element => element.name == string)
+};
