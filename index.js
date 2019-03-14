fa6581e30b44f04ff24464abea0f330627046d34
Maximo Bautista <max.bautista97@gmail.com>
2019-03-14 14:00:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const findMatching = (array, string) => {
   return array.filter(element => element.toLowerCase() == string.toLowerCase() );
 };
+
+const findMatching = (array, string) => {
+  return array.filter(element => element.toLowerCase() == string.toLowerCase() );
+};
